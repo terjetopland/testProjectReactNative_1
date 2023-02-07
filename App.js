885fb3d76e36af13a875117e22d6b36be4966bb3
Terje Topland <terjetop@uia.no>
2023-02-07 15:38:24 +0100

    Added button with alert/modal

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+    Alert,
+    Button,
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+} from 'react-native';
 
-export default function App() {
+const Header1 = () =>
+    (
+        <Text style={styles.title}>Welcome to my android app!</Text>
+    );
+
+const Separator = () => <View style={styles.separator} />;
+
+const ALittleButton = () =>
+    (
+        <Button
+            title="Press me"
+            onPress={() => Alert.alert('Simple Button pressed')}
+        />
+    );
+
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header1/>
+      <Text>Testing stuff</Text>
+      <Separator/>
+      <Separator/>
+      <ALittleButton/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8ef',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    textAlign: 'center',
+    textShadowColor: 'black',
+    marginVertical: 50,
+    fontSize: 20,
+    fontStyle: 'italic',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
 });
+
+export default App;
